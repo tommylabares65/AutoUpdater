bbba500d3ba86cb452224577a3898eb2022d2142
tommylabares65 <40931949+tommylabares65@users.noreply.github.com>
2018-08-31 08:49:28 +0800

    Update and rename update.js to update.json

--- update.js
+++
@@ -1,9 +0,0 @@
-{
-	"versionCode":10,
-	"versionName":"1.0.0",
-	"fileSize":"35.93M",
-	"apkUrl":"https://www.datafilehost.com/d/9c69da03",
-	"required":false,
-	"releaseDate":"2017-02-12 12:45:20",
-	"releaseNotes":["新版本特性新版本特性新版本特性；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
-}
